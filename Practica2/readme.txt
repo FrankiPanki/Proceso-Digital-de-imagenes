85550b4541a47dcccd4fa1ac63ca6ebad4630fd4
FrankiPanki <puchicher@gmail.com>
2019-08-23 21:24:23 -0500

    Practica2

---
+++ Practica2/readme.txt
@@ -0,0 +1,36 @@
+Nombre Francisco Daniel Gamez Garcia
+no cuenta 312215176
+
+Instrucciones:
+Por razones de los navegadores, la aplicación solo puede ser abierta mediante el uso de un servidor,
+la manera en la que estuve realizando el desarrollo es a travez del editor de texto visual studio code
+https://code.visualstudio.com/
+
+y se levantaba la aplicación a travez del plugin para el mismo editor
+https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
+
+solo hay que instalare el editor y luego abrir él link, aceptar y finalmente instalar.
+
+o en su casos buscar el plugin dentro del editor.
+
+Dejo un pequeño tutorial en la carpeta que empieza después de instalar visual studio code, en la parte donde se instala el plugin debería de aparecer instalar, solo que yo ya lo tengo instalado.
+
+
+La parte de brillo , tamaño del mosaico, los parámetros los paso por default como 5 y 10, si se quiere cambiar, solo hay que cambiar los valores que están en la llamada a la función 
+  this.model.practica1.mosaico(5);
+Y 
+    this.model.practica1.brillo(10);
+
+que están dentro del archivo Controller.js
+
+La parte donde están los algoritmos de los filtros se encuentran dentro del archivo Practica1.js 
+
+
+Cualquier duda este es mi correo.
+
+
+
+franciscogamez@ciencias.unam.mx
+
+
+Pd, utilizar de preferencia el navegador Mozilla
---
+++ Practica2/src/View.js
@@ -0,0 +1,99 @@
+export default class View {
+  constructor() { 
+  }
+
+  init(controller) {
+    this.controller = controller;
+
+
+    // CARGA DE IMAGEN
+
+    this.canvas = document.getElementById("drawing");
+    this.work_area = document.getElementById("work_area");
+
+    document.getElementById("btn_open").addEventListener("click", () => {
+      this.controller.open();
+    });
+
+    document.getElementById("btn_save").addEventListener("click", () => {
+      this.controller.save();
+    });
+
+    document.getElementById("btn_undo").addEventListener("click", () => {
+      this.controller.undo();
+    });
+
+
+
+    // PRACTICA1-----------------------------------------------
+
+    document.getElementById("btn_grayscale").addEventListener("click", () => {
+      this.controller.grayscale_action();
+    });
+
+    document.getElementById("btn_invert").addEventListener("click", () => {
+      this.controller.invert_action();
+    });
+
+    document.getElementById("btn_red_channel").addEventListener("click", () => {
+      this.controller.red_channel_action();
+    });
+
+    document.getElementById("btn_green_channel").addEventListener("click", () => {
+      this.controller.green_channel_action();
+    });
+    
+    document.getElementById("btn_blue_channel").addEventListener("click", () => {
+      this.controller.blue_channel_action();
+    });
+    document.getElementById("btn_mosaico").addEventListener("click", () => {
+      this.controller.mosaico_action();
+    });
+    document.getElementById("btn_brillo").addEventListener("click", () => {
+      this.controller.brillo_action();
+    });
+    document.getElementById("btn_alto_contraste").addEventListener("click", () => {
+      this.controller.alto_contraste_action();
+    });
+
+
+    // PRACTICA2-----------------------------------------------
+    document.getElementById("btn_blur").addEventListener("click", () => {
+      this.controller.blur();
+    });
+    document.getElementById("btn_motion_blur").addEventListener("click", () => {
+      this.controller.motion_blur();
+    });
+    document.getElementById("btn_encontrar_bordes").addEventListener("click", () => {
+      this.controller.encontrar_bordes();
+    });
+    document.getElementById("btn_sharpen").addEventListener("click", () => {
+      this.controller.sharpen();
+    });
+    document.getElementById("btn_emboss").addEventListener("click", () => {
+      this.controller.emboss();
+    });
+    document.getElementById("btn_promedio").addEventListener("click", () => {
+      this.controller.promedio();
+    });
+    document.getElementById("btn_mediana").addEventListener("click", () => {
+      this.controller.mediana();
+    });
+
+    
+  }
+
+  setTitle(tit) {
+    let title = document.querySelector("title");
+    if (!title) {
+      title = document.createElement("title");
+      document.head.appendChild(title);
+    }
+
+    title.textContent = tit;
+  }
+
+  showCanvas() {
+    this.canvas.style.display = "block";
+  }
+}
